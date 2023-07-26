eeeca2eb6fad85e22a81a8463ed7b78c449d7885
[Chiam Ramsey] <[ 139248653+ChiamRamsey-2006@users.noreply.github.com]>
2023-07-26 14:11:21 -0400

    creating add button

--- src/MyFirstUiProvider.tsx
+++ src/MyFirstUiProvider.tsx
@@ -4,9 +4,34 @@ import {
   UiItemsProvider,
   Widget,
 } from '@itwin/appui-react';
-import DebugWidget  from './components/DebugWidget';
+import React from 'react';
+import { KeySet } from '@itwin/presentation-common';
+import { ISelectionProvider, Presentation, SelectionChangeEventArgs } from '@itwin/presentation-frontend';
+import { text } from 'stream/consumers';
+import { LabeledInput } from '@itwin/itwinui-react';
+import Number3 from './components/number3';
 
+const DebugWidget = () => {
+const [selectedElements, setSelectedElements] = React.useState<Readonly<KeySet>>(new KeySet([]));
 
+const _onSelectionChanged=(evt: SelectionChangeEventArgs, selectionProvider: ISelectionProvider) => {
+    const selection = selectionProvider.getSelection(evt.imodel, evt.level);
+    setSelectedElements(new KeySet(selection));
+  };
+  Presentation.selection.selectionChange.addListener(_onSelectionChanged);
+
+  const selected: JSX.Element[] = [];
+  selectedElements.instanceKeys.forEach((value: Set<string>, key: string) => {
+    const elements:JSX.Element[] = [];
+    value.forEach((v: string) => {elements.push(<li>{v}</li>)})
+    selected.push(<div>{key}:<ul>{elements}</ul></div>);
+  })
+
+  return (<div>
+    {selected}
+  </div> 
+  )
+}
 export class MyFirstUiProvider implements UiItemsProvider {
   public readonly id = 'HelloWorldProvider';
   public provideWidgets(
@@ -23,6 +48,15 @@ export class MyFirstUiProvider implements UiItemsProvider {
     content: <DebugWidget/>
     }
     widgets.push(DW);
-    return widgets;
+
+    const AB: Widget = {
+      id: 'Add Button',
+      label:'Add Button', 
+      content: <Number3/>
+      }
+      widgets.push(AB);
+      return widgets;
+
   }
+
 }
