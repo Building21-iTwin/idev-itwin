eec53f255cb7ea8e32ea41bb63cb805951bc3e86
Jordan savage <102739683+DJmonkboi@users.noreply.github.com>
2023-08-01 13:33:45 -0400

    geese

--- src/MyFirstUiProvider.tsx
+++ src/MyFirstUiProvider.tsx
@@ -67,6 +67,7 @@ const EW: Widget = {
   label: 'EmphasizeWidget',
   content: <Emphasizewidget/>
 }
+
 widgets.push(EW)
 
     return widgets;
--- src/components/Emphasizewidget.tsx
+++ src/components/Emphasizewidget.tsx
@@ -1,6 +1,27 @@
- const Emphasizewidget =()=> {
+import { ColorDef } from "@itwin/core-common/lib/cjs/ColorDef";
+import { ColorPickerButton } from "@itwin/imodel-components-react/lib/cjs/imodel-components-react/color/ColorPickerButton";
+import { Button, Checkbox, Flex, LabeledInput } from "@itwin/itwinui-react";
+import React from "react";
+import Querycompt from "./Querycompt";
 
-        return (<div></div>);
-    };
-    export default Emphasizewidget
-    
+
+const Emphasizewidget = () => {
+
+    return <div>
+        <Flex flexDirection="column" >
+            <Checkbox />
+            <ColorPickerButton initialColor={ColorDef.blue} />
+            <Button>Remove</Button>
+            <LabeledInput />
+            <Button>Add</Button>
+            <Button>Emphasize</Button>
+            <Button>Clear</Button>
+            
+        </Flex>
+        <Querycompt />
+    </div>
+
+};
+
+
+export default Emphasizewidget
