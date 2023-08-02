a94622907992fd0aca43ac7e055d064e078f9c34
[Chiam Ramsey] <[ 139248653+ChiamRamsey-2006@users.noreply.github.com]>
2023-08-02 15:10:09 -0400

    remove for now

--- src/components/Emphasizewidget.tsx
+++ src/components/Emphasizewidget.tsx
@@ -1,15 +1,47 @@
 import React from "react";
-import { Button, Checkbox, Flex, LabeledInput } from "@itwin/itwinui-react";
+import { Button, Flex } from "@itwin/itwinui-react";
 import { ColorDef } from "@itwin/core-common/lib/cjs/ColorDef";
-import { ColorPickerButton } from "@itwin/imodel-components-react/lib/cjs/imodel-components-react/color/ColorPickerButton";
-import Querycompt from "./querycompt";
-const Emphasizewidget = () => {
+import Querycompt, { Queryprops } from "./querycompt";
 
+const Emphasizewidget = () => {
+const [queryList, SetQueryList] = React.useState<Queryprops[]>([]);
+const [count, setCount] = React.useState<number>(0);
+ 
+function addQuery(_event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void{
+    const newQueryList = [...queryList];
+    newQueryList.push({id: count, enabled: false, color: ColorDef.blue, query:""});
+    SetQueryList(newQueryList);
+    setCount(count+1);
+}
+function queryChanged(_newProps: Queryprops): void {
+    const newList = queryList.map((q: Queryprops) => {
+        if (q.id === _newProps.id) {
+            return _newProps;
+        }
+        return q;
+    });
+    SetQueryList(newList);
+}
+
+function removedclick(id: number): void {
+ const newList = queryList.filter ((q: Queryprops) => {
+    return q.id !== id;
+    });
+    SetQueryList(newList);
+}
+const queryElements: JSX.Element[] = [];
+queryList.forEach((p: Queryprops) => {
+    queryElements.push(<Querycompt props={p} handleChange={queryChanged} removeClick={removedclick} />)
+});
     return (<div>
-        <Button>Add</Button>
+       <Button onClick={addQuery}>Add</Button>
         <Button>Emphasize</Button>
         <Button>Clear</Button>
-        <Querycompt />
+        <Flex flexDirection="column">
+            {queryElements}
+        </Flex>
     </div>);
 };
 export default Emphasizewidget
+
+
--- src/components/querycompt.tsx
+++ src/components/querycompt.tsx
@@ -1,23 +1,68 @@
 import * as React from 'react';
 import { Button, Textarea } from '@itwin/itwinui-react';
 import { Checkbox, Flex } from '@itwin/itwinui-react';
-import { ColorPickerButton } from '@itwin/imodel-components-react';
 import { ColorDef } from '@itwin/core-common';
+import { ColorPickerButton } from '@itwin/imodel-components-react';
 
-const Querycompt = () => {
-    const [value, setvalue] = React.useState<string>("");
+
+
+export interface Queryprops {
+    id: number;
+    enabled: boolean;
+    color: ColorDef;
+    query: string;
+}
+
+export interface QueryComponentProps {
+    props: Queryprops;
+    handleChange(newProps: Queryprops): void;
+    removeClick(id: number): void;
+}
+
+const Querycompt = ({
+    props,
+    handleChange,
+    removeClick
+}: QueryComponentProps) => {
+    const [value, setvalue] = React.useState<string>(props.query);
+    const [checkBoxChecked, setCheckBoxChecked] = React.useState<boolean>(props.enabled);
+    const [color, setColor] = React.useState<ColorDef> (props.color);
+
+    function remove(_event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+        removeClick(props.id)
+    };
+
+    function checked(event: React.ChangeEvent<HTMLInputElement>): void {
+        props.enabled = event.target.checked;
+        handleChange(props);
+        setCheckBoxChecked(event.target.checked);
+    }
+
+    function queryChanged(event: React.ChangeEvent<HTMLTextAreaElement>): void {
+        setvalue(event.target.value);
+        handleChange(props);
+        props.query = event.target.value;
+    }
+
+    function colorChanged(color: ColorDef): void {
+        props.color = color;
+        handleChange(props);
+        setColor(color);
+    }
+
     return (<div>
         <Flex>
-            <Checkbox />
-            <ColorPickerButton initialColor={ColorDef.blue} />
+            <Checkbox checked={checkBoxChecked} onChange={checked}/>
+            <Button onClick={remove}>Remove</Button>
+            <ColorPickerButton initialColor={ColorDef.blue} onColorPick={colorChanged}/>
             <Textarea
                 id='text-area'
                 value={value}
-                onChange={(event) => setvalue(event.target.value)}
+                onChange={queryChanged}
                 style={{ width: '70%' }}
             />
         </Flex>
     </div>)
 };
-export default Querycompt
 
+export default Querycompt
