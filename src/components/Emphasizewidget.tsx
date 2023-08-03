d807582e1773c206f1f8a9625046311c65b0debc
Hassan Durant <139161166+hassandurant@users.noreply.github.com>
2023-08-03 10:40:18 -0400

    Grim

--- src/components/Emphasizewidget.tsx
+++ src/components/Emphasizewidget.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Checkbox, Flex, LabeledInput } from "@itwin/itwinui-react";
 import { ColorDef } from "@itwin/core-common/lib/cjs/ColorDef";
 import { ColorPickerButton } from "@itwin/imodel-components-react/lib/cjs/imodel-components-react/color/ColorPickerButton";
-import Querycompt from "./Querycompt";
+import Querycompt from "./querycompt";
 const Emphasizewidget = () => {
 
     return (<div>
